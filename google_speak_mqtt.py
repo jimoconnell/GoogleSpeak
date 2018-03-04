a92aaed1b2827ebc0c6a70981a2634d9fedfdf5f
Jim O'Connell <jim@mmdc.net>
2018-03-04 12:18:29 -0500

    Moving settings to config.ini

--- google_speak_mqtt.py
+++ google_speak_mqtt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Modified by @jimoconnell to add Google Say functionality via Home Assistant
 # See: https://gist.github.com/jimoconnell/35d7a23a492ca5499c71b5465c6173df
@@ -22,8 +22,17 @@
 #import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.client as mqtt
 import requests
+from configparser import SafeConfigParser
 
+parser = SafeConfigParser()
+parser.read('config.ini')
 
+HOMEASSISTANT_URL =  parser.get('config', 'HOMEASSISTANT_URL')
+CAST_DEVICE =  parser.get('config', 'CAST_DEVICE')
+MQTT_SERVER =  parser.get('config', 'MQTT_SERVER')
+MQTT_TOPIC =  parser.get('config', 'MQTT_Topic')
+
+print("Using server: " + MQTT_SERVER)
 
 def on_connect(mqttc, obj, flags, rc):
     print("Connected to %s:%s" % (mqttc._host, mqttc._port))
@@ -34,8 +43,9 @@ def on_message(mqttc, obj, msg: object):
     print(str(msg.payload))
 
     saymsg = str(msg.payload)
-    data = "{\"entity_id\": \"media_player.studio_speaker\", \"message\": \"" + saymsg + "\"}"
-    r = requests.post("http://your_homeassistant:8123/api/services/tts/google_say?api_password=YOUR_PASSWORD", data)
+    data = "{\"entity_id\": \""+ CAST_DEVICE + "\", \"message\": \"" + saymsg + "\"}"
+    data = "{ \"message\": \"" + saymsg + "\"}"
+    r = requests.post(HOMEASSISTANT_URL, data)
 def on_publish(mqttc, obj, mid):
     print("mid: "+str(mid))
 
@@ -55,9 +65,9 @@ mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
-mqttc.connect_srv("mosquitto.org", 60)
-mqttc.subscribe("jimoconnell/googlesay/messages", 0)
+mqttc.on_log = on_log
+mqttc.connect_srv(MQTT_SERVER, 60)
+mqttc.subscribe(MQTT_TOPIC, 0)
 
 rc = 0
 while rc == 0:
